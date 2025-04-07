e8966591396c78aacff776766396bd58069419ca
SiriJV <siri.viren@icloud.com>
2025-04-07 09:33:02 +0200

    Added Color Options Panel

---
+++ src/components/ColorOptions/ColorOptions.tsx
@@ -0,0 +1,19 @@
+import './ColorOptions.scss';
+
+const ColorOptions = () => {
+    return (
+        <article className="colors">
+          <div className="color"></div>
+          <div className="color"></div>
+          <div className="color"></div>
+          <div className="color"></div>
+          <div className="delete">DEL</div>
+          <div className="color"></div>
+          <div className="color"></div>
+          <div className="color"></div>
+          <div className="enter">ENTER</div>
+        </article>
+    )
+}
+
+export default ColorOptions;
