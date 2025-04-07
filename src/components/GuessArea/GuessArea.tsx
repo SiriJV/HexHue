7f2c9202b2871930b679b105b1076d48124e71c5
SiriJV <siri.viren@icloud.com>
2025-04-07 09:33:15 +0200

    Added Guess Area

---
+++ src/components/GuessArea/GuessArea.tsx
@@ -0,0 +1,46 @@
+import './GuessArea.scss';
+
+const GuessArea = () => {
+    return (
+<article className="guesses">
+        <div className="row1">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="circle">?</div>
+        </div>
+        <div className="row2">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="circle">?</div>
+        </div>
+        <div className="row3">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="circle">?</div>
+        </div>
+        <div className="row4">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="circle">?</div>
+        </div>
+        <div className="row5">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="circle">?</div>
+        </div>
+        <div className="row6">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="circle">?</div>
+        </div>
+        </article>
+    )
+}
+
+export default GuessArea;
